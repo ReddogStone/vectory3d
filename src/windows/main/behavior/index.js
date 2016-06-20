96a4cd29bcebc3283ff34f1b06aa22f9e608a9c5
Reddogstone <thereddog@gmx.net>
2016-06-20 19:17:12 +0200

    Renamed create behavior to console

--- src/windows/main/behavior/index.js
+++ src/windows/main/behavior/index.js
@@ -5,13 +5,13 @@ const assert = require('assert');
 const Behavior = require('../../../framework/behavior');
 
 const CameraBehavior = require('./camera');
-const CreateBehavior = require('./create');
+const ConsoleBehavior = require('./console');
 const HighlightBehavior = require('./highlight');
 
 module.exports = function(canvas, state, hitTest, actions) {
 	return Behavior.first(
 		CameraBehavior(canvas, state, actions),	
-		CreateBehavior(state),
+		ConsoleBehavior(state),
 		HighlightBehavior(canvas, state, hitTest, actions)
 	);
 };
